6b0c7b4d8ae78e14af7b7c2e59e1f2740121ca67
Robin Bakker <robinb@live.nl>
2024-12-09 22:10:38 +0100

    8 dec - Part 1 finished
    With a little help we found the bug. Edgecase that was acting weird. Also cleaned up code a bit, so got a bit faster. Part 2 to be continued...

--- 8 dec/8 dec/Program.cs
+++ 8 dec/8 dec/Program.cs
@@ -25,6 +25,7 @@ namespace _8_dec
 
             watch.Stop(); // Stop stopwatch and print
             Console.WriteLine("Execution time: " + watch.ElapsedMilliseconds);
+            Console.ReadLine();
         }
 
         static void puzzle1()
@@ -50,47 +51,34 @@ namespace _8_dec
             {
                 for (byte x = 0; x < matrixArray.GetLength(1); x++) // loop through all positions on row
                 {
-                    if (y == baseCoord[0] && x == 0 && baseCoord[1]+1 < matrixArray.GetLength(1)) // We can ignore all positions on row before antenna, so if on same row and X is before antenna X
+
+                    if (y == baseCoord[0] && x == 0)
                     {
-                        x = (byte)(baseCoord[1] +1); // set to check X to X after antenna X
+                        if (baseCoord[1] + 1 < matrixArray.GetLength(1)) // We can ignore all positions on row before antenna, so if on same row and X is before antenna X
+                        {
+                            x = (byte)(baseCoord[1] + 1); // set to check X to X after antenna X
+                        }
+                        else // if current antenna is last place on line, then goto next line first place.
+                        {
+                            y++;
+                        }
                     }
+
                     if (matrixArray[y, x] == currentFrequency) // Found other antenna
                     {
                         int diffY = y - baseCoord[0]; // calculate difference in rows/Y
                         int diffX = x - baseCoord[1]; // calculate difference in positions/X
                         int[] node1 = {y + diffY, x + diffX }; // Calculate the coordinates of one other antinode
                         int[] node2 = { baseCoord[0] - diffY, baseCoord[1] - diffX }; // Calculate the coordinates of one other antinode
-                        try // try-catch because when out of bounds we want to ignore
+
+                        if (node1[0] < matrixArray.GetLength(0) && node1[1] < matrixArray.GetLength(1) && node1[0] >= 0 && node1[1] >= 0)
                         {
-                            //antinodes.Add((node1[0], node1[1]));
-                            if (matrixArray[node1[0], node1[1]].Equals('.')) // When found antinode is still empty
-                            {
-                                matrixArray[node1[0], node1[1]] = '#';
-                                antinodes.Add((node1[0], node1[1]));
-                            }
-                            else // When found antinode is not empty (when another antenna, add it to antinodes. When other antinode it doesn't add (hashmap)
-                            {
-                                antinodes.Add((node1[0], node1[1]));
-                            }
+                            antinodes.Add((node1[0], node1[1]));
                         }
-                        catch // when out of bounds
+
+                        if (node2[0] < matrixArray.GetLength(0) && node2[0] >= 0 && node2[1] < matrixArray.GetLength(1) && node2[1] >= 0)
                         {
-                        }
-                        try // try-catch because when out of bounds we want to ignore
-                        {
-                            //antinodes.Add((node2[0], node2[1]));
-                            if (matrixArray[node2[0], node2[1]].Equals('.')) // When found antinode is still empty
-                            {
-                                matrixArray[node2[0], node2[1]] = '#';
-                                antinodes.Add((node2[0], node2[1]));
-                            }
-                            else
-                            {
-                                antinodes.Add((node2[0], node2[1]));
-                            }
-                        }
-                        catch // When out of bounds
-                        {
+                            antinodes.Add((node2[0], node2[1]));
                         }
                     }
                 }
@@ -100,7 +88,7 @@ namespace _8_dec
 
         static void setup()
         {
-            string[] lines = File.ReadAllLines("C:\\Users\\Robin\\Documents\\GitHub\\AdventOfCode2024\\8 dec\\input.txt");
+            string[] lines = File.ReadAllLines("C:\\Users\\bakr\\OneDrive - Driestar-Wartburg\\Documents\\GitHub\\AdventOfCode2024\\8 dec\\input.txt");
             int rows = lines.Length;
             int cols = lines[0].Length;
             matrixArray = new char[rows, cols];
